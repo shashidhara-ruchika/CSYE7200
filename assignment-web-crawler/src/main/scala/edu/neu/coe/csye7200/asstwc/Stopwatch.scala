2bc5852b2a4a3b683955d062c357e3aaed05fa5e
Robin Hillyard <r.hillyard@northeastern.edu>
2024-10-11 13:17:25 -0400

    Added Stopwatch to WebCrawler

---
+++ assignment-web-crawler/src/main/scala/edu/neu/coe/csye7200/asstwc/Stopwatch.scala
@@ -0,0 +1,43 @@
+package edu.neu.coe.csye7200.asstwc
+
+import scala.language.implicitConversions
+
+/**
+ * Class to time processes
+ */
+class Stopwatch extends AutoCloseable {
+
+  /**
+   * Return the elapased time of this lap (in milliseconds)
+   *
+   * @return a Long value of milliseconds
+   */
+  def lap: Int = {
+    if (lapStart == 0) throw StopwatchException("Stopwatch has stopped")
+    val current = System.nanoTime()
+    val result = current - lapStart
+    lapStart = current
+    result // implicitly converts to milliseconds
+  }
+
+  def stop: (Int,Int) = {
+    if (lapStart == 0) throw StopwatchException("Stopwatch has stopped")
+    val current = System.nanoTime()
+    val lapTime = current - lapStart
+    val totalTime = current - start
+    lapStart = 0L
+    (lapTime, totalTime) // implicitly converts to milliseconds
+  }
+
+  def close(): Unit = {
+    if (lapStart != 0) throw StopwatchException("Stopwatch.close(): still running")
+  }
+
+  private val start: Long = System.nanoTime()
+  private var lapStart: Long = start // NOTE this is a var
+  private val MILLION = 1_000_000
+
+  private implicit def convertToMilliseconds(nanos: Long): Int = (nanos / MILLION).toInt
+}
+
+case class StopwatchException(msg: String) extends Exception(msg)
