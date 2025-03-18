1de5a3942cf7fd6b8c93f800660ed09385d6fbb9
Ruchika-Shashidhara-002245068 <144740650+shashidhara-ruchika@users.noreply.github.com>
2025-03-18 13:51:15 -0400

    Revert "Lab Work"

--- lab-99/src/main/scala/edu/neu/coe/csye7200/lab99/scala99/Sieve.scala
+++
@@ -1,9 +0,0 @@
-package edu.neu.coe.csye7200.lab99.scala99
-
-object Sieve extends App {
-    val primes: LazyList[Int] = 2 #:: LazyList.from(3, 2).filter(isPrime(_))
-
-    def isPrime(x: BigInt): Boolean = (x > 1) && primes.takeWhile(_ <= Math.sqrt(x.toDouble)).forall(x % _ != 0)
-
-    println(primes take 100_000 to List)
-}
